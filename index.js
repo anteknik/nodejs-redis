20a466ff075e9af868b0686b6eb486b90baae15e
antonpurwanto <klinikeuangan@gmail.com>
2018-08-25 05:17:28 +0700

    add redis url ignore pdf kit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const PDFDocument = require('pdfkit')
+//const PDFDocument = require('pdfkit')
 const PORT = process.env.PORT || 5000
 const app = express()
 
