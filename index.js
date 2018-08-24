0a75ca5fd6dd717501f090adf101f5c9f9cba49e
antonpurwanto <klinikeuangan@gmail.com>
2018-08-25 02:14:40 +0700

    init nodejs web heroku

---
+++ index.js
@@ -0,0 +1,11 @@
+var http = require("http");
+
+var port = Number(process.env.PORT || 5000);
+
+http.createServer(function(request, response){
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello Altomatik");
+    response.end();
+}).listen(port);
+
+console.log("App ready at port "+port);
