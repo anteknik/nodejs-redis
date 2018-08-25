0b16e2d4a5e48861e0e5b64c97bff1906e09a50d
antonpurwanto <klinikeuangan@gmail.com>
2018-08-25 21:52:12 +0700

    add field message redis

--- index.js
+++ index.js
@@ -4,56 +4,78 @@ const PDFDocument = require('pdfkit')
 const PORT = process.env.PORT || 5000
 const app = express()
 
-app.get('/', (req, res) => res.send('<h2> Hello Altomatik! </h2>'))
+	var REDISTOGO_URL = "redis://redistogo:e013a4d1c249922d9835fe68ae0ac666@tetra.redistogo.com:9180/"
+    var rtg   = require("url").parse(REDISTOGO_URL);
+    var redis = require("redis").createClient(rtg.port, rtg.hostname);
+    redis.auth(rtg.auth.split(":")[1])
+        
+	var fs = require('fs')
+    var doc = new PDFDocument()
+    doc.text('Hello Altomatik')
+    
+    var displayContent = function(){
+        redis.get("nama", function(err, reply){
+            console.log("Nama : "+reply)
+            var nama = doc.text('Nama : '+reply)
+            redis.expire("nama", 1)    
+        })
+
+        redis.get("email", function(err, reply){
+            console.log("Email : "+reply)
+            var nama = doc.text('Email : '+reply)
+            redis.expire("email", 1)    
+        })
+		
+		redis.get("message", function(err, reply){
+            console.log("Mesagge : "+reply)
+            var nama = doc.text('Message : '+reply)
+            redis.expire("message", 1)    
+        })
+    }
+
+    redis.on("connect", function(){
+        redis.set("nama", "Anton Purwanto")
+        redis.set("email", "anton.takaful@gmail.com")
+		redis.set("message", "belajar setting environment nodejs dan redis")
+        displayContent();
+    })
+
+
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-var REDISTOGO_URL = "redis://redistogo:e013a4d1c249922d9835fe68ae0ac666@tetra.redistogo.com:9180/";
+app.get('/', (req, res) =>{ 
+    res.send('<h2>Hello Altomatik </h2><a href="/my/pdf">Download Pdf</a> ')
+    console.log('hello')
+})
 
-var rtg   = require("url").parse(REDISTOGO_URL);
-var redis = require("redis").createClient(rtg.port, rtg.hostname);
-redis.auth(rtg.auth.split(":")[1]);
+app.get('/my/pdf', (req, res) =>{
 
-var displayContent = function(){
-    redis.get("nama", function(err, reply){
-        console.log("Nama : "+reply);
-        redis.expire("nama", 1);    
-    });
-
-    redis.get("email", function(err, reply){
-        console.log("Email : "+reply);
-        redis.expire("email", 1);    
-    });
-};
-
-redis.on("connect", function(){
-    redis.set("nama", "Anton Purwanto");
-    redis.set("email", "anton.takaful@gmail.com");
-    displayContent();
-});
-
-//var fs = require('fs');
-
-//var pdf = new PDFDocument({
-  //size: 'LEGAL', 
-  //info: {
-    //Title: 'Tile of File Here',
-    //Author: 'Some Author',
-  //}
-//});
-
-// Write stuff into PDF
-//pdf.text('Hello Altomatik ');
-//pdf.text('Hello Altomatik');
-
-// Stream contents to a file
-//pdf.pipe(
-  //fs.createWriteStream('file.pdf')
-//)
-  //.on('finish', function () {
-    //console.log('PDF closed');
-  //});
-
+    console.log('disi ini fungsi buat pdf ')
+    //doc.pipe(res)
+    doc.pipe(
+        fs.createWriteStream('report/hello-altomatik.pdf')  
+      )
+        .on('finish', function () {
+          console.log('PDF created');
+        })
+	
 // Close PDF and write file.
-//pdf.end();
-  
-  
+    doc.end()
+    res.status(200).send('<h4><a href="/my/pdf/view">Lihat File</a></h4>')
+})
+
+app.get('/my/pdf/view', (req, res) =>{
+
+    //res.setHeader('Content-disposition', 'attachment; filename=hello.pdf')
+	//res.set('Content-Type', 'text/pdf')
+    var filepath = "report/hello-altomatik.pdf"
+    //res.download('report/hello-altomatik.pdf')
+    fs.readFile(filepath , function (err,data){
+        res.contentType("application/pdf")
+    res.send(data)
+    console.log('PDF view or dowload');
+})
+    
+
+})
+
